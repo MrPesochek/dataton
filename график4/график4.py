ae4d85850a7acd1849042655390b35aaebc73ec9
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:18:46 +0300

    Delete график4 directory

--- график4/график4.py
+++
@@ -1,7 +0,0 @@
-plt.figure(figsize=(10, 6))
-sns.barplot(data=df_long_grouped, x='grouped_age', y='count', hue='ad_type')
-plt.xlabel('Age Group')
-plt.ylabel('Count')
-plt.title('Count useful actions by age group')
-plt.legend(title='Advertisement Type', loc='upper right')
-plt.show()
