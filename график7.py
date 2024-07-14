000023a61651a32e933e2e47427bc831f15ec554
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:22:17 +0300

    Update график7.py

--- график7.py
+++ график7.py
@@ -13,4 +13,15 @@ merged_df = merged_df.rename(columns={
     "marketing_company_outcome_2": "ad_2",
     "marketing_company_outcome_3": "ad_3",
     "marketing_company_outcome_4": "ad_4",
-    "marketing_company_outcome_5": "ad_5",
+    "marketing_company_outcome_5": "ad_5",})
+new_df = merged_df[["grouped_age", "ad_1", "ad_2", "ad_3", "ad_4", "ad_5"]]
+df_grouped = new_df.groupby('grouped_age').sum().reset_index()
+df_long_grouped = df_grouped.melt(id_vars=['grouped_age'], value_vars=['ad_1', 'ad_2', 'ad_3', 'ad_4', 'ad_5'], 
+                                  var_name='ad_type', value_name='count')
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df_long_grouped, x='grouped_age', y='count', hue='ad_type')
+plt.xlabel('Age Group')
+plt.ylabel('Count')
+plt.title('Advertisement Counts by Age Group')
+plt.legend(title='Advertisement Type', loc='upper right')
+plt.show()
