c6742102935549a521449af1d3cad90be4c29703
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:24:55 +0300

    Update график2.py

--- график2.py
+++ график2.py
@@ -1,3 +1,9 @@
+#Импорт библиотек
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 plt.scatter(data=userdata, x="age", y="income", color='green', alpha=0.3)
 plt.xlabel("Age")
 plt.ylabel("Income")
