84791c0e8bef71a677de6d46d683635fa42623e5
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:25:33 +0300

    Update график8.py

--- график8.py
+++ график8.py
@@ -1,3 +1,9 @@
+#Импорт библиотек
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 df = interactiondata.groupby('grouped_date')['complain_count'].sum().reset_index()
 sns.barplot(x='grouped_date', y='complain_count', data=df)
 plt.xlabel('Last order date')
