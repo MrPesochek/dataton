385c0fb0e9e6f12695970422ddbfd9c21ad46409
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:23:57 +0300

    Create график8.py

---
+++ график8.py
@@ -0,0 +1,7 @@
+df = interactiondata.groupby('grouped_date')['complain_count'].sum().reset_index()
+sns.barplot(x='grouped_date', y='complain_count', data=df)
+plt.xlabel('Last order date')
+plt.ylabel('Complains count')
+plt.title('Complain counts by last order date')
+plt.xticks(rotation=45)
+plt.show()
