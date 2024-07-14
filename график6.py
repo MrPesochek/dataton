55c90e3eaeec2f3529e679d7afa4d15a9a579f70
MrPesochek <97695810+MrPesochek@users.noreply.github.com>
2024-07-14 15:25:13 +0300

    Update график6.py

--- график6.py
+++ график6.py
@@ -1,3 +1,9 @@
+#Импорт библиотек
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 plt.figure(figsize=(10, 6))
 sns.barplot(data=df_long_grouped, x='grouped_age', y='count', hue='ad_type')
 plt.xlabel('Age Group')
